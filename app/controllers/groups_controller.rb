f41d483e047b6d9c40be3086a939873abce422f0
BenWu <hidesor@yahoo.com.tw>
2016-05-27 22:57:25 +0800

    討論版加發布者

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,6 +12,7 @@ class GroupsController < ApplicationController
     def show
         @group = Group.find(params[:id])
         @posts = @group.posts
+        @user = @group.owner
     end
 
     def edit

23d72d734ea8fcc5c93f1a1c05505c72af5e0ad2
BenWu <hidesor@yahoo.com.tw>
2016-06-04 13:32:36 +0800

    產品分類OK1

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,7 @@ class Product < ActiveRecord::Base
   belongs_to :type
   accepts_nested_attributes_for :photo
   self.per_page = 10
+  validates :price, numericality: true
+  validates :quantity, numericality: true
+  #validates_presence_of :price
 end
